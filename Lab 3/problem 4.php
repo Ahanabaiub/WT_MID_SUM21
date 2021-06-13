ce68885d9dcfb9b0908017271f74bbbcbd25180b
Ahanab Shakil <shieam47@gmail.com>
2021-06-13 15:02:04 +0600

    problem 4 added

---
+++ Lab 3/problem 4.php
@@ -0,0 +1,12 @@
+<?php
+
+    $name = "raju";
+
+    if($name=="raju" || $name=="mina" || $name=="mithu"){
+        echo "Hello $name";
+    }
+    else{
+        echo "You are not recognized.";
+    }
+
+?>
